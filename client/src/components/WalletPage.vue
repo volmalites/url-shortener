1b01dc276254adcfb4cc3bbd581c4cc68e2eed47
Bjorn Lottering <bjornlottering@Bjorns-Mac-mini.local>
2025-10-08 16:02:37 +0200

    Adjusted information displayed to data returned by wallet endpoint

--- client/src/components/WalletPage.vue
+++ client/src/components/WalletPage.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="wallet">
 		<h3>Wallet</h3>
-		<p v-if="wallet">Balance: R{{ wallet.balance?.toFixed(2) || '0.00' }}</p>
-		<p v-if="wallet">Current Percentage: {{ wallet.percentage }}%</p>
+		<p v-if="wallet">Total earnings: R {{ wallet.totalBalance?.toFixed(2) || '0.00' }}</p>
+		<p v-if="wallet">Your balance: R {{ wallet.balance?.toFixed(2) || '0.00' }}</p>
 		<p v-if="wallet">{{ wallet.growthInfo }}</p>
+		<p v-if="wallet">Current total clicks: {{ wallet.totalClicks || 0 }}</p>
 		<p v-else>Loading wallet...</p>
 	</div>
 </template>
@@ -13,7 +14,7 @@ export default {
 	props: {
 		wallet: {
 			type: Object,
-			default: () => ({ Balance: 0, Percentage: 10, GrowthInfo: '' })
+			default: () => ({ Balance: 0, TotalBalance: 0, Percentage: 10, GrowthInfo: '', TotalClicks: 0 })
 		}
 	}
 };
