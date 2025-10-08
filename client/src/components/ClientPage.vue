6c18cd0ad37e8ca7c9a19fe83d1a0c52dd4cff01
Bjorn Lottering <bjornlottering@Bjorns-Mac-mini.local>
2025-10-08 09:33:31 +0200

    User information

---
+++ client/src/components/ClientPage.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h3>Wallet</h3>
+		<p>Balance: R{{ wallet.Balance.toFixed(2) }}</p>
+		<p>Current Percentage: {{ wallet.Percentage }}%</p>
+		<p>{{ wallet.GrowthInfo }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		wallet: Object
+	}
+};
+</script>
